1e222bac5e27104f75de3c67a21d9ee94d9882b2
Edwin23-crypto <edwinchimborazo71@gmail.com>
2024-10-30 14:15:36 -0500

    Modif
    Modificacion

---
+++ funcion.py
@@ -0,0 +1,20 @@
+def suma(n1, n2):
+  return n1+n1
+
+def restar(n1,n2):
+  return n1 - n2
+
+def multiplicar(n1, n2):
+  return n1 * n2
+
+def dividir(n1, n2):
+  return n1 / n2
+
+n1= int(input("Ingresa el numero 1:"))
+n2= int(input("Ingresa el numero 2:"))
+
+print("La suma es:", suma(n1, n2))
+print("La resta es:", restar(n1, n2))
+print("La multiplicacion es:", multiplicar(n1, n2))
+print("La division es:", dividir(n1, n2))
+
